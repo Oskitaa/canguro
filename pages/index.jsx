b4c9efc1531ed08e4a2d596ccd710f733dca5c41
Óscar <ese.betico.98@gmail.com>
2021-05-17 21:21:16 +0200

    login y reset password

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,8 @@
 import Nav from '/components/nav'
 import Bottom from '/components/bottom'
-import useUser from '/hooks/useUser'
+import useUser , { USER_STATES } from '/hooks/useUser'
 import {auth} from '/firebase/client'
+
 export default function Home(){
     
     const user = useUser()
@@ -10,8 +11,8 @@ export default function Home(){
         <html>
             <Nav/>
             <Bottom/>
-            <button onClick={()=>{console.log(auth.currentUser);}}>User</button>
-            <button onClick={()=>{auth.signOut()}}>SingOut</button>
+            <button onClick={()=>{console.log(auth.currentUser)}}>User</button>
+            {user !== USER_STATES.NOT_LOGGED && (<button onClick={()=>{auth.signOut()}}>SingOut</button>)}
             <style jsx>{`
                 html{
                     height:200vh;
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,26 +1,65 @@
-import {log, auth} from '/firebase/client'
+import { log } from '/firebase/client'
+import Link from 'next/link'
 import { useRouter } from "next/router"
+import { Form, Button } from 'react-bootstrap';
+import { useState } from 'react';
 
-
-export default function Login(){
+export default function Login() {
     const router = useRouter()
-    const onSubmit = (e) => {
-        
-        e.preventDefault()
-        const { email, password} = e.target
-        log(email.value,password.value)
+    const [validated, setValidated] = useState(false);
+    const onSubmit = (event) => {
+        event.preventDefault()
+        const form = event.currentTarget;
+        const { email, password } = event.target
+        form.checkValidity() && log(email.value, password.value)
+        setValidated(true)
+        console.log(validated)
         router.push("/")
     }
 
-    return(
-        <>
-            <h1>Estas en singup</h1>
-            <form onSubmit={onSubmit}>
-                <input type="text" name="email"/>
-                <input type="password" name="password"/>
-                <button type="submit">Enviar</button>
-            </form>
-            <button onClick={()=>console.log(auth.currentUser)}>Clic</button>
-        </>
+    return (
+        <div className="container">
+            <h1>Inicio de sesión</h1>
+            <div className="dialog">
+                <Form noValidate onSubmit={onSubmit} validated={validated}>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Correo electronico</Form.Label>
+                        <Form.Control required type="email" placeholder="Email" name="email" min="3"/>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Contraseña</Form.Label>
+                        <Form.Control type="password" placeholder="Contraseña" name="password" required min="6" max="8"/>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Text><Link href="/forgotpassword">Has olvidado tu contraseña?</Link></Form.Text>
+                    </Form.Group>
+                    <Button variant="primary" type="submit" className="myBtn">
+                        Iniciar sesión
+                    </Button>
+                </Form>
+            </div>
+            <style type="text/css">{`
+                h1{
+                    margin-bottom: 15px;
+                }
+                .btn-primary{
+                    width:100%;
+                }
+                .dialog{
+                    width:400px;
+                    padding:20px;
+                    border-radius:10px;
+                    box-shadow:0px 0px 5px #333;
+                }
+                .container{
+                    display:flex;
+                    flex-direction:column;
+                    justify-content:center;
+                    align-items:center;
+                    height:100vh;
+                }
+            `}</style>
+
+        </div>
     )
 }

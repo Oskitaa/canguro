4e0f8c7bad3fcef01fcd7182a6d6135cd018715f
Óscar <ese.betico.98@gmail.com>
2021-06-06 00:20:52 +0200

    head

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
+import Head from "next/head";
 import Feed from "/components/feed";
-import {logo} from "/constant/logo"
+import { logo } from "/constant/logo";
 
 export default function Home() {
   return (
-  <>
-  <meta property="og:description" content="Encuentra a tu canguro ideal en tan solo 3 minutos!"/>
+    <>
+      <Head>
+        <meta property="og:description"content="Encuentra a tu canguro ideal en tan solo 3 minutos!"/>
         <meta property="og:site_name" content="CangurApp" />
         <meta property="og:image" content={logo} />
         <meta property="og:image:secure_url" content={logo} />
@@ -12,7 +14,8 @@ export default function Home() {
         <meta property="og:image:width" content="400" />
         <meta property="og:image:height" content="300" />
         <meta property="og:image:alt" content="Imagen del logo" />
-  <Feed />
-  </>
-    )
+      </Head>
+      <Feed />
+    </>
+  );
 }

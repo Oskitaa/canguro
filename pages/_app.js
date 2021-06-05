9329e273f1e9ca4dc56b0b1c89fcacbbd55d06f3
Óscar <ese.betico.98@gmail.com>
2021-06-05 18:33:36 +0200

    cambio og

--- pages/_app.js
+++ pages/_app.js
@@ -13,12 +13,12 @@ function MyApp({ Component, pageProps }) {
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous"></link>
         <meta property="og:description" content="Sean Connery found fame and fortune as the suave, sophisticated British agent, James Bond."/>
         <meta property="og:site_name" content="CangurApp" />
-        <meta property="og:image" content={loog} />
+        <meta property="og:image" content={logo} />
         <meta property="og:image:secure_url" content={logo} />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="400" />
         <meta property="og:image:height" content="300" />
-        <meta property="og:image:alt" content="Imagen del logo" />
+        <meta property="og:image:alt" content="Imagen del logp" />
       </Head>
       <Menu />
       <Component {...pageProps} />
